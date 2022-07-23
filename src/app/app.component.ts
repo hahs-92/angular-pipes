9585906791ff0bf27fcc6aa5594e113b834ea834
hahs <alexandre_2420@outlook.com>
2022-07-23 08:46:31 -0500

    [added]: setup primeNg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'pipesApp';
+  name = 'alex hernandez';
+
+  showName() {
+    this.name;
+  }
 }
+
+//135
