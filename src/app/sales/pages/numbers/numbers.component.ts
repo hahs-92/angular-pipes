cfef3eeb2d100c3b5a011e2dde22b74affa1810f
hahs <alexandre_2420@outlook.com>
2022-07-23 15:48:25 -0500

    [added]: numbers pipes

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NumbersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NumbersComponent {
+  netSales = 23434374.5667;
+  percentage = 0.1956;
 }
